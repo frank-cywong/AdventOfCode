8de47a175284019c904ba36815c731f45792355b
frank-cywong <90653148+frank-cywong@users.noreply.github.com>
2021-11-09 15:28:08 -0500

    Day 3 Part 2

--- 2015/Three.java
+++ 2015/Three.java
@@ -8,18 +8,33 @@ public class Three{
 		String path = Files.readString(Paths.get(args[0]));
 		int curx = 0;
 		int cury = 0;
+		int curx2 = 0;
+		int cury2 = 0;
 		visited.add(new IntPair(curx, cury));
 		for(int i = 0; i < path.length(); i++){
-			if(path.charAt(i) == '^'){
-				cury++;
-			} else if (path.charAt(i) == 'v'){
-				cury--;
-			} else if (path.charAt(i) == '<'){
-				curx--;
-			} else if (path.charAt(i) == '>'){
-				curx++;
+			if(i % 2 == 0){
+				if(path.charAt(i) == '^'){
+					cury++;
+				} else if (path.charAt(i) == 'v'){
+					cury--;
+				} else if (path.charAt(i) == '<'){
+					curx--;
+				} else if (path.charAt(i) == '>'){
+					curx++;
+				}
+				visited.add(new IntPair(curx,cury));
+			} else {
+				if(path.charAt(i) == '^'){
+					cury2++;
+				} else if (path.charAt(i) == 'v'){
+					cury2--;
+				} else if (path.charAt(i) == '<'){
+					curx2--;
+				} else if (path.charAt(i) == '>'){
+					curx2++;
+				}
+				visited.add(new IntPair(curx2,cury2));
 			}
-			visited.add(new IntPair(curx,cury));
 		}
 		System.out.println(visited.size());
 	}
