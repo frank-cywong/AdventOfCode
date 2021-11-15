3f17062d29f17386735a8f9abdaa35bea2b87036
frank-cywong <90653148+frank-cywong@users.noreply.github.com>
2021-11-15 11:47:33 -0500

    Add basic files for Day 7

---
+++ 2015/SevenTreeNode.java
@@ -0,0 +1,8 @@
+public class SevenTreeNode{
+  public int l_node, r_node, mode;
+  public SevenTreeNode(int l_node, int r_node, int mode){
+    this.l_node = l_node;
+    this.r_node = r_node;
+    this.mode = mode;
+  }
+}
