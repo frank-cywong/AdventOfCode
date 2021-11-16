9b0436895efc99401a00f07f4584d4852998a58b
frank-cywong <90653148+frank-cywong@users.noreply.github.com>
2021-11-16 14:50:28 -0500

    Day 7 Part 1 More Node Operations

--- 2015/SevenTreeNode.java
+++ 2015/SevenTreeNode.java
@@ -1,8 +1,46 @@
 public class SevenTreeNode{
-  public int l_node, r_node, mode;
-  public SevenTreeNode(int l_node, int r_node, int mode){
+  private SevenTreeNode l_node, r_node;
+  private short mode, value;
+  private boolean valueset;
+  public SevenTreeNode(SevenTreeNode l_node, SevenTreeNode r_node, short mode){
     this.l_node = l_node;
     this.r_node = r_node;
     this.mode = mode;
+    this.valueset = false;
+  }
+  public SevenTreeNode(short value){
+    this.value = value;
+    this.valueset = true;
+  }
+  public short getValue(){
+    if(valueset){
+      return value;
+    }
+    switch (mode){
+      case 1: // AND
+        value = l_node.getValue() & r_node.getValue();
+        break;
+      case 2: // OR
+        value = l_node.getValue() | r_node.getValue();
+        break;
+      case 3: // LSHIFT
+        value = l_node.getValue() << r_node.getValue();
+        break;
+      case 4: // RSHIFT
+        value = l_node.getValue() >>> r_node.getValue(); // unsigned right shift
+        break;
+      case 5: // NOT
+        value = ~l_node.getValue();
+        break;
+      valueset = true;
+      return value;
+    }
+  }
+  public static int unsignedShortToSignedInt(short i){
+    if(i >= 0){
+      return (int)i;
+    } else {
+      return(65536 - (int)i);
+    }
   }
 }
