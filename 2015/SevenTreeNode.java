0f9d7f42785572293ff213169fd89286002b4794
frank-cywong <90653148+frank-cywong@users.noreply.github.com>
2021-11-17 14:05:55 -0500

    Change short to char to avoid signed issues

--- 2015/SevenTreeNode.java
+++ 2015/SevenTreeNode.java
@@ -1,46 +1,40 @@
 public class SevenTreeNode{
   private SevenTreeNode l_node, r_node;
-  private short mode, value;
+  private int mode;
+  private char value;
   private boolean valueset;
-  public SevenTreeNode(SevenTreeNode l_node, SevenTreeNode r_node, short mode){
+  public SevenTreeNode(SevenTreeNode l_node, SevenTreeNode r_node, int mode){
     this.l_node = l_node;
     this.r_node = r_node;
     this.mode = mode;
     this.valueset = false;
   }
-  public SevenTreeNode(short value){
+  public SevenTreeNode(char value){
     this.value = value;
     this.valueset = true;
   }
-  public short getValue(){
+  public char getValue(){
     if(valueset){
       return value;
     }
     switch (mode){
       case 1: // AND
-        value = l_node.getValue() & r_node.getValue();
+        value = (char)(l_node.getValue() & r_node.getValue());
         break;
       case 2: // OR
-        value = l_node.getValue() | r_node.getValue();
+        value = (char)(l_node.getValue() | r_node.getValue());
         break;
       case 3: // LSHIFT
-        value = l_node.getValue() << r_node.getValue();
+        value = (char)(l_node.getValue() << r_node.getValue());
         break;
       case 4: // RSHIFT
-        value = l_node.getValue() >>> r_node.getValue(); // unsigned right shift
+        value = (char)(l_node.getValue() >>> r_node.getValue()); // unsigned right shift
         break;
       case 5: // NOT
-        value = ~l_node.getValue();
+        value = (char)(~l_node.getValue());
         break;
-      valueset = true;
-      return value;
     }
-  }
-  public static int unsignedShortToSignedInt(short i){
-    if(i >= 0){
-      return (int)i;
-    } else {
-      return(65536 - (int)i);
-    }
+    valueset = true;
+    return value;
   }
 }
