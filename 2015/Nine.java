021044a327696b022f7e2fbbc0db977aa73211fc
frank-cywong <90653148+frank-cywong@users.noreply.github.com>
2021-11-23 14:51:10 -0500

    Base code for day 9

---
+++ 2015/Nine.java
@@ -0,0 +1,7 @@
+import java.io.*;
+import java.util.scanner;
+public class Nine{
+  public static void main(String[] args){
+    
+  }
+}
---
+++ 2015/NineGraphNode.java
@@ -0,0 +1,5 @@
+import javafx.util.Pair;
+public class NineGraphNode{
+  public String name;
+  public Pair<String, Integer>[] paths;
+}
