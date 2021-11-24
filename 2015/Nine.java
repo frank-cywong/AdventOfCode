face70682e95b306894fb2a7396523f9678ad296
frank-cywong <90653148+frank-cywong@users.noreply.github.com>
2021-11-24 14:50:45 -0500

    Day Nine starting implmentation of recursion

--- 2015/Nine.java
+++ 2015/Nine.java
@@ -1,7 +1,52 @@
 import java.io.*;
-import java.util.scanner;
+import java.util.*;
 public class Nine{
-  public static void main(String[] args){
-    
+  static Map<String,NineGraphNode> nodes = new HashMap<String,NineGraphNode>();
+  public static int iteratenodes(Set<String> nodesleft, int curminpath, int pathuntilnow, String prevnode){
+    String[] nodesleftlist = (String[])nodesleft.toArray();
+    Set<String> temp;
+    int temp2;
+    for(int i = 0; i < nodesleftlist.length; i++){
+      temp.addAll(nodesleft);
+      temp.remove(nodes.get(nodesleftlist[i]));
+      if(prevnode != null){
+        pathuntilnow += nodes.get(prevnode).paths.get(nodesleftlist[i]);
+      }
+      temp2 = iteratenodes(temp,curminpath,pathuntilnow,nodesleftlist[i]);
+      if(temp2 < curminpath){
+        curminpath = temp2;
+      }
+    }
+    return curminpath;
+  }
+  public static void main(String[] args) throws FileNotFoundException{
+    File f = new File(args[0]);
+    Scanner in = new Scanner(f);
+    String temp;
+    String[] temp2;
+    String[] temp3;
+    NineGraphNode tempnode;
+    while (Scanner.hasNextLine()){
+      temp = Scanner.nextLine();
+      temp2 = temp.split(" to ");
+      temp3 = new String[3];
+      temp3[0] = temp2[0];
+      temp2 = temp2[1].split(" = ");
+      temp3[1] = temp2[0];
+      temp3[2] = temp2[1];
+      tempnode = nodes.get(temp3[0]);
+      if(tempnode == null){
+        tempnode = new NineGraphNode(temp3[0]);
+        nodes.put(temp3[0],tempnode);
+      }
+      tempnode.addPath(temp3[1],Integer.parseInt(temp3[2]));
+      tempnode = nodes.get(temp3[1]);
+      if(tempnode == null){
+        tempnode = new NineGraphNode(temp3[1]);
+        nodes.put(temp3[1],tempnode);
+      }
+      tempnode.addPath(temp3[0],Integer.parseInt(temp3[2]));
+    }
+    System.out.println(iteratenodes(nodes.keySet(),Integer.MAX_VALUE,0,null));
   }
 }
