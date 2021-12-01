c3c9de531547d0481ea814a1a09d24184a28bf38
frank-cywong <90653148+frank-cywong@users.noreply.github.com>
2021-12-01 09:07:21 -0500

    Day 1 Part 2

--- 2021/One.java
+++ 2021/One.java
@@ -4,13 +4,17 @@ public class One{
   public static void main(String[] args) throws FileNotFoundException{
     File f = new File(args[0]);
     Scanner in = new Scanner(f);
-    int last;
+    int threebefore = -1;
+    int twobefore = -1;
+    int onebefore = -1;
     int cur = -1;
     int output = 0;
     while(in.hasNextInt()){
-      last = cur;
+      threebefore = twobefore;
+      twobefore = onebefore;
+      onebefore = cur;
       cur = in.nextInt();
-      if(cur > last && last >= 0){
+      if(cur > threebefore && threebefore >= 0){
         output++;
       }
     }
