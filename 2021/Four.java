008568e072c692dd2d10f173a021d39a308e49d8
frank-cywong <90653148+frank-cywong@users.noreply.github.com>
2021-12-04 11:36:32 -0500

    Day 4 Part 2

--- 2021/Four.java
+++ 2021/Four.java
@@ -1,18 +1,21 @@
 import java.io.*;
 import java.util.*;
 public class Four{
+  static boolean[] won; // if board has won, for part 2
+  static int lastwon;
   static long[] v; // stores in bitwise form for if a number was called
   static ArrayList<int[][]> boarddata = new ArrayList<int[][]>();
   static long[] wins = {0b11111L, 0b11111L << 5, 0b11111L << 10, 0b11111L << 15, 0b11111L << 20, 0b1000010000100001000010000L, 0b0100001000010000100001000L, 0b0010000100001000010000100L, 0b0001000010000100001000010L, 0b0000100001000010000100001L};
-  public static int hasWin(){
+  public static ArrayList<Integer> hasWin(){
+    ArrayList<Integer> o = new ArrayList<Integer>();
     for(int i = 0; i < v.length; i++){
       for(int j = 0; j < wins.length; j++){
         if((v[i] & wins[j]) == wins[j]){
-          return i;
+          o.add(i);
         }
       }
     }
-    return 0;
+    return o;
   }
   public static int computeScore(int index, int last){
     int count = 0;
@@ -64,23 +67,35 @@ public class Four{
       }
     }
     String[] numstring = numbers.split(",");
-    int win = 0;
+    ArrayList<Integer> win;
     int cur = 0;
     v = new long[boarddata.size()];
+    won = new boolean[boarddata.size()];
     for(int i = 0; i < numstring.length; i++){
       cur = Integer.parseInt(numstring[i]);
       for(int j = 0; j < v.length; j++){
+        if(won[j]){
+          continue;
+        }
         for(int k = 0; k < 25; k++){
           if(boarddata.get(j)[k / 5][k % 5] == cur){
             v[j] |= (1L << k);
           }
         }
       }
+      System.out.println(cur);
       win = hasWin();
-      if(win != 0){
-        System.out.println(computeScore(win, cur));
-        break;
+      if(win.size() != 0){
+        for(int j = 0; j < win.size(); j++){
+          if(won[win.get(j)]){
+            continue;
+          }
+          won[win.get(j)] = true;
+          lastwon = computeScore(win.get(j),cur);
+          System.out.println(win.get(j) + " has won");
+        }
       }
     }
+    System.out.println(lastwon);
   }
 }
