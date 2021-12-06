19cf7ffafdb6e7d9d7e16806d846e1e392166cc9
frank-cywong <90653148+frank-cywong@users.noreply.github.com>
2021-12-06 09:07:11 -0500

    Day 6 part 2

--- 2021/Six.java
+++ 2021/Six.java
@@ -1,6 +1,7 @@
 import java.io.*;
 import java.util.*;
 public class Six{
+  /* part 1
   static ArrayList<Integer> d = new ArrayList<Integer>();
   public static void incday(){
     for(int i = 0; i < d.size(); i++){
@@ -12,18 +13,40 @@ public class Six{
       d.set(i, d.get(i) - 1);
     }
   }
+  */
+  static long[] d = new long[9];
+  public static void incday(){
+    long zero = 0;
+    for(int i = 0; i < d.length; i++){
+      if(i == 0){
+        zero = d[i];
+      }
+      if(i == 8){
+        d[i] = zero;
+        continue;
+      }
+      d[i] = d[i+1];
+      if(i == 6){
+        d[i] += zero;
+      }
+    }
+  }
   public static void main(String[] args) throws FileNotFoundException{
     File f = new File(args[0]);
     Scanner in = new Scanner(f);
     String s = in.nextLine();
     String[] temp = s.split(",");
     for(int i = 0; i < temp.length; i++){
-      d.add(Integer.parseInt(temp[i]));
+      d[Integer.parseInt(temp[i])]++;
     }
     int turns = Integer.parseInt(args[1]);
     for(int i = 0; i < turns; i++){
       incday();
     }
-    System.out.println(d.size());
+    long count = 0;
+    for(int i = 0; i < d.length; i++){
+      count += d[i];
+    }
+    System.out.println(count);
   }
 }
