cebe4629fb00ac5a6c569d7ec361f41c420d22ce
frank-cywong <90653148+frank-cywong@users.noreply.github.com>
2021-12-07 14:43:48 -0500

    Day 11

--- 2015/Eleven.java
+++ 2015/Eleven.java
@@ -6,24 +6,23 @@ public class Eleven{
   public static boolean isIncSubString(String s){
     return((s.charAt(1)-1 == s.charAt(0)) && (s.charAt(2)-1 == s.charAt(1)));
   }
+  public static boolean isPair(String s){
+    return(s.charAt(0) == s.charAt(1));
+  }
   public static boolean isValid(String p){
-    Set<String> twolist = new HashSet<String>();
-    String twolast = null;
-    String twocur = null;
     int hasPairCount = 0;
     boolean hasIncreasing = false;
+    boolean nocheck = false;
     for(int i = 0; i < p.length(); i++){
       if(isInvalidChar(p.charAt(i))){
         return false;
       }
       if((hasPairCount < 2) && i < p.length() - 1){
-        if(twolast != null){
-          twolist.add(twolast);
-        }
-        twolast = twocur;
-        twocur = p.substring(i, i+2);
-        if(twolist.contains(twocur)){
+        if(nocheck){
+          nocheck = false;
+        } else if (isPair(p.substring(i,i+2))){
           hasPairCount++;
+          nocheck = true;
         }
       }
       if((!hasIncreasing) && i < p.length() - 2){
@@ -50,7 +49,7 @@ public class Eleven{
     String temp = args[0];
     while(!isValid(temp)){
       temp = incStr(temp);
-      System.out.println(temp);
+      //System.out.println(temp);
     }
     System.out.println(temp);
   }
