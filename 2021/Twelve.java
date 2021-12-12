a5df67313a3abadd9d5cf74188c7875010af9e6a
frank-cywong <90653148+frank-cywong@users.noreply.github.com>
2021-12-12 10:01:39 -0500

    Day Twelve Part 2

--- 2021/Twelve.java
+++ 2021/Twelve.java
@@ -3,16 +3,21 @@ import java.io.*;
 public class Twelve{
   static HashMap<String, Integer> visited = new HashMap<String, Integer>();
   static HashMap<String, Set<String>> nodes = new HashMap<String, Set<String>>();
+  //static ArrayList<String> path = new ArrayList<String>();
   static int pathcount = 0;
+  static boolean smallplus = false;
   public static boolean isBig(String node){
     return(Character.isUpperCase(node.charAt(0)));
   }
   public static void dfs(String node){
     if(node.equals("end")){
       pathcount++;
+      //System.out.println(path.toString());
       return;
     }
     //System.out.println(node);
+    //path.add(node);
+    //System.out.println(path.toString());
     Set<String> adj = nodes.get(node);
     if(visited.containsKey(node)){
       visited.put(node, visited.get(node) + 1);
@@ -23,11 +28,19 @@ public class Twelve{
     String tempnode;
     while(it.hasNext()){
       tempnode = it.next();
+      if(tempnode.equals("start")){
+        continue;
+      }
       if(isBig(tempnode) || !visited.containsKey(tempnode) || visited.get(tempnode) == 0){
         dfs(tempnode);
+      } else if (!smallplus && visited.get(tempnode) == 1){
+        smallplus = true;
+        dfs(tempnode);
+        smallplus = false;
       }
     }
     visited.put(node, visited.get(node) - 1);
+    //path.remove(path.size()-1);
   }
   public static void main(String[] args) throws FileNotFoundException{
     File f = new File(args[0]);
