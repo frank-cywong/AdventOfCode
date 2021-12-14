99913683ea52626cb74d06218557e7920b2c1431
frank-cywong <90653148+frank-cywong@users.noreply.github.com>
2021-12-14 11:28:44 -0500

    Day 14 Part 2

--- 2021/FourteenTwo.java
+++ 2021/FourteenTwo.java
@@ -4,14 +4,42 @@ public class FourteenTwo{
   static HashMap<String, Long> freqcount = new HashMap<String, Long>();
   static HashMap<String, String[]> rules = new HashMap<String, String[]>();
   static Set<String> substrings;
+  static String polymer = null;
   public static void doStep(){
     HashMap<String, Long> newfreqcount = new HashMap<String, Long>();
-    
+    Iterator<String> it = substrings.iterator();
+    String key;
+    String[] newkeys;
+    long freq;
+    while(it.hasNext()){
+      key = it.next();
+      newkeys = rules.get(key);
+      freq = freqcount.getOrDefault(key, 0L);
+      newfreqcount.put(newkeys[0], newfreqcount.getOrDefault(newkeys[0], 0L) + freq);
+      newfreqcount.put(newkeys[1], newfreqcount.getOrDefault(newkeys[1], 0L) + freq);
+    }
+    freqcount = newfreqcount;
   }
   public static long countFreq(){
-    Long[] values = freqcount.values().toArray(new Long[0]);
+    Iterator<String> it = substrings.iterator();
+    String key;
+    HashMap<String, Long> charfreqcount = new HashMap<String, Long>();
+    String ckey;
+    long freq;
+    charfreqcount.put(polymer.substring(0,1),1L); // for double-counting compensation
+    charfreqcount.put(polymer.substring(polymer.length()-1, polymer.length()), 1L);
+    while(it.hasNext()){
+      key = it.next();
+      freq = freqcount.getOrDefault(key, 0L);
+      for(int i = 0; i < key.length(); i++){
+        ckey = key.substring(i, i+1);
+        charfreqcount.put(ckey, freq + charfreqcount.getOrDefault(ckey, 0L));
+      }
+    }
+    System.out.println(charfreqcount);
+    Long[] values = charfreqcount.values().toArray(new Long[0]);
     Arrays.sort(values);
-    return(values[values.length-1] - values[0]);
+    return((values[values.length-1] - values[0]) / 2); // due to double-counting
   }
   public static void main(String[] args) throws FileNotFoundException{
     File f = new File(args[0]);
@@ -19,7 +47,7 @@ public class FourteenTwo{
     String temp;
     String[] rule;
     String[] inputpair;
-    String polymer = in.nextLine();
+    polymer = in.nextLine();
     while(in.hasNextLine()){
       temp = in.nextLine();
       if(temp.equals("")){
@@ -28,7 +56,7 @@ public class FourteenTwo{
       rule = temp.split(" -> ");
       inputpair = new String[2];
       inputpair[0] = rule[0].substring(0,1) + rule[1];
-      inputpair[1] = rule[1].substring(1,2) + rule[1];
+      inputpair[1] = rule[1] + rule[0].substring(1,2);
       rules.put(rule[0], inputpair);
     }
     substrings = rules.keySet();
@@ -40,6 +68,7 @@ public class FourteenTwo{
     }
     for(int i = 0; i < count; i++){
       doStep();
+      //System.out.println(freqcount);
       //System.out.println("Doing step: "+i);
     }
     System.out.println(countFreq());
